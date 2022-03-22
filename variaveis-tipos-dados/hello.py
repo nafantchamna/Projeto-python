a442f2da2955f95ebcc4d4f58cd8b1ed23f87687
Cletche <cletche91@hotmail.com>
2022-03-22 19:40:25 -0300

    Primeiro commit de exercicios de python utilizando visual code

---
+++ variaveis-tipos-dados/hello.py
@@ -0,0 +1,14 @@
+for l in "Olá mundo!":
+ print("L")   
+    
+    
+    [2] if "L" in "Lucas":
+    print("L")
+    
+    for l in "Ola, mundo!":
+        print(l)
+        
+    for a in range(20):
+        print(a)    
+        
+        
